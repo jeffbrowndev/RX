5bf3036c6e00c654ef74239a2c6988d484f92bee
Jeff Brown <jeffbrowndev@gmail.com>
2018-11-26 11:34:59 -0800

    Setup variables, font imports, and basic footer functionality

--- keystone.js
+++ keystone.js
@@ -3,19 +3,20 @@ var keystone = require('keystone');
 // Initialize keystone
 keystone.init({
   'cookie secret': 'secure string goes here',
-  'name': 'RX',
+  name: 'RX',
   'user model': 'User',
   'auto update': true,
-  'auth': true,
+  auth: true,
   views: 'templates/views',
-  'view engine': 'pug'
+  'view engine': 'pug',
+  'static': 'public',
 });
 
 // Import models
 keystone.import('models');
 
 // Set location for routes
-keystone.set('routes', require('./routes'))
+keystone.set('routes', require('./routes'));
 
 // Start keystone
 keystone.start();
